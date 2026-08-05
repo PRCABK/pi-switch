2be20583d2e9eeaa2e93f24b3bdec40945b8b2ab
DaShan <2601720800@qq.com>
2026-08-05 13:25:09 +0800

    界面优化

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,32 @@ import { ChatDotRound, Connection, Setting } from "@element-plus/icons-vue";
   <div class="app-shell">
     <aside class="sidebar">
       <div class="brand">
-        <div class="brand-mark">π</div>
-        <div>
+        <div class="brand-mark" aria-hidden="true">π</div>
+        <div class="brand-copy">
           <strong>Pi Switch</strong>
-          <small>模型与对话管理</small>
+          <small>Control Center</small>
         </div>
       </div>
+
+      <div class="nav-label">工作台</div>
       <el-menu router :default-active="$route.path" class="nav-menu">
         <el-menu-item index="/models"><el-icon><Connection /></el-icon><span>模型管理</span></el-menu-item>
         <el-menu-item index="/sessions"><el-icon><ChatDotRound /></el-icon><span>对话管理</span></el-menu-item>
         <el-menu-item index="/settings"><el-icon><Setting /></el-icon><span>应用设置</span></el-menu-item>
       </el-menu>
-      <div class="sidebar-footer">Pi Switch v0.1.0</div>
+
+      <div class="sidebar-footer">
+        <span class="status-dot"></span>
+        <span>Pi Switch</span>
+        <span class="version">v0.2.1</span>
+      </div>
     </aside>
-    <main class="main-content"><router-view /></main>
+    <main class="main-content">
+      <router-view v-slot="{ Component }">
+        <transition name="page-fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </main>
   </div>
 </template>
